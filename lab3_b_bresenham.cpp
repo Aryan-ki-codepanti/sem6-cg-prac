bec666fd0e6d83e07a1e9ee3c6a6800d27702d86
Aryan Sethi <aryan.rdps@gmail.com>
2024-01-31 18:01:32 +0530

    lab3_b_bresenham.cpp

--- lab3_b_bresenham.cpp
+++ lab3_b_bresenham.cpp
@@ -9,7 +9,11 @@ void bresenham(float start_x, float start_y, float end_x, float end_y)
 
     // handle vertical case
     if (end_x == start_x)
+    {
+        for (float i = start_y; i <= end_y; i++)
+            putpixel(start_x, i, GREEN);
         return;
+    }
 
     float del_x = end_x - start_x;
     float del_y = end_y - start_y;
@@ -20,7 +24,7 @@ void bresenham(float start_x, float start_y, float end_x, float end_y)
 
     float x = start_x, y = start_y;
 
-    while ((x < end_x) && (y < end_y))
+    while ((x <= end_x) && (y <= end_y))
     {
         cout << x << " , " << y << endl;
         putpixel(x, y, RED);
@@ -40,7 +44,15 @@ int main()
     int gd = DETECT, gm;
     initgraph(&gd, &gm, (char *)"");
 
+    // 0 to 1 case
     bresenham(20, 10, 60, 100);
+
+    // vertical case
+    bresenham(20, 10, 20, 100);
+
+    // horizontal case
+    bresenham(20, 10, 80, 10);
+
     getch();
     closegraph();
     return 0;
