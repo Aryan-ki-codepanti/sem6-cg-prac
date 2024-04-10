0610be87a62f47022ea435cc5cdeb7e355b6eb00
Aryan Sethi <aryan.rdps@gmail.com>
2024-04-10 22:08:23 +0530

    Line clipped

--- line_clip.cpp
+++ line_clip.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <graphics.h>
 #include <vector>
 using namespace std;
 
@@ -6,27 +7,14 @@ class line_clip
 {
 
 public:
-    vector<vector<float>> points;
-    vector<string> codes;
     float xmax, xmin, ymax, ymin;
-    int n;
 
-    line_clip(int n, float xmax, float xmin, float ymax, float ymin)
+    line_clip(float xmax, float xmin, float ymax, float ymin)
     {
         this->xmax = xmax;
         this->xmin = xmin;
         this->ymax = ymax;
         this->ymin = ymin;
-        this->n = n;
-    }
-
-    void input_coords()
-    {
-        float a, b;
-        cout << "Enter points" << endl;
-
-        for (int i = 0; i < n; i++)
-            points.push_back({a, b});
     }
 
     string compute_code(vector<float> pt)
@@ -37,26 +25,118 @@ public:
         code += to_string(pt[1] < ymin); // Bottom
         code += to_string(pt[0] > xmax); // Right
         code += to_string(pt[0] < xmin); // left
-        cout << code << endl;
         return code;
     }
 
-    // void line_clip(vector<float> a, vector<float> b)
-    // {
-    // }
+    vector<vector<float>> clip_line(vector<float> a, vector<float> b)
+    {
+        string c1 = this->compute_code(a);
+        string c2 = this->compute_code(b);
+
+        // C1 or C2 = 0000 (both 0)
+        string res_or = compute_or(c1, c2);
+        string res_and = compute_and(c1, c2);
+        if (res_or == "0000")
+            return {a, b};
+
+        // completely outside window and ignore
+        if (res_and != "0000")
+            return {{-99999}, {-99999}};
+
+        // clipping line
+        float slope = (b[1] - a[1]) / (b[0] - a[0]);
+
+        // TBRL check
+        // top intersection
+        vector<vector<float>> clipped;
+
+        float temp_x, temp_y;
+
+        if (res_or[0] == '1') // intersection on top
+        {
+            temp_x = a[0] + (ymax - a[1]) / slope;
+            clipped.push_back({temp_x, ymax});
+        }
+        if (res_or[1] == '1') // intersection on bottom
+        {
+            temp_x = a[0] + (ymin - a[1]) / slope;
+            clipped.push_back({temp_x, ymin});
+        }
+        if (res_or[2] == '1') // intersection on right
+        {
+            temp_y = a[1] + (xmax - a[0]) * slope;
+            clipped.push_back({xmax, temp_y});
+        }
+        if (res_or[3] == '1') // intersection on left
+        {
+            temp_y = a[1] + (xmin - a[0]) * slope;
+            clipped.push_back({xmin, temp_y});
+        }
+
+        return clipped;
+    }
+
+    static string compute_and(string c1, string c2)
+    {
+        string res;
+        int a, b;
+        for (int i = 0; i < 4; i++)
+        {
+            a = c1[i] - '0';
+            b = c2[i] - '0';
+            res += to_string(a & b);
+        }
+        return res;
+    }
+
+    static string compute_or(string c1, string c2)
+    {
+        string res;
+        int a, b;
+        for (int i = 0; i < 4; i++)
+        {
+            a = c1[i] - '0';
+            b = c2[i] - '0';
+            res += to_string(a | b);
+        }
+        return res;
+    }
+
+    static void print_line(vector<vector<float>> pts)
+    {
+        cout << pts[0][0] << " , " << pts[0][1] << endl;
+        cout << pts[1][0] << " , " << pts[1][1] << endl;
+    }
 };
 
 int main()
 {
-    int n;
-    // cout << "Enter number of points : ";
-    // cin >> n;
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char *)"");
 
-    line_clip P = line_clip(n, 2, -3, 6, 1);
+    float x_wmin = 20, x_wmax = 90, y_wmin = 20, y_wmax = 70;
 
-    P.compute_code({-4, 2});
-    P.compute_code({-1, 7});
-    // cout << (char)(-4 > 1) << endl;
+    line_clip P = line_clip(x_wmax, x_wmin, y_wmax, y_wmin);
+
+    vector<float> A = {10, 30};
+    vector<float> B = {80, 90};
+
+    setcolor(3);
+    line(A[0], A[1], B[0], B[1]);
+    rectangle(x_wmin, y_wmax, x_wmax, y_wmin);
+
+    setcolor(14);
+    vector<vector<float>> clipped = P.clip_line(A, B);
+
+    if (clipped[0].size() == 1)
+    {
+        cout << "line outside window" << endl;
+        return 0;
+    }
+
+    line(clipped[0][0], clipped[0][1], clipped[1][0], clipped[1][1]);
+
+    getch();
 
     return 0;
 }
