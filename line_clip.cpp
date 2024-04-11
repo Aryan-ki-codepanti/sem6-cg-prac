e69866a093b3ff0b837e92b5027413683c6697b2
Aryan Sethi <aryan.rdps@gmail.com>
2024-04-11 11:29:37 +0530

    Line clippin added

--- line_clip.cpp
+++ line_clip.cpp
@@ -28,58 +28,85 @@ public:
         return code;
     }
 
-    vector<vector<float>> clip_line(vector<float> a, vector<float> b)
+    void clip_line(vector<float> a, vector<float> b)
     {
         string c1 = this->compute_code(a);
         string c2 = this->compute_code(b);
 
-        // C1 or C2 = 0000 (both 0)
-        string res_or = compute_or(c1, c2);
-        string res_and = compute_and(c1, c2);
+        bool done = false, accept = false;
 
-
-        if (res_or == "0000")
-            return {a, b};
-
-        // completely outside window and ignore
-        if (res_and != "0000")
-            return {{-99999}, {-99999}};
-
-        // clipping line
-        float slope = (b[1] - a[1]) / (b[0] - a[0]);
-
-        // TBRL check
-        // top intersection
-        vector<vector<float>> clipped;
-
-        float temp_x, temp_y;
-
-        if (res_or[0] == '1') // intersection on top
+        do
         {
-            cout << "Top" << endl;
-            temp_x = a[0] + (ymax - a[1]) / slope;
-            clipped.push_back({temp_x, ymax});
-        }
-        if (res_or[1] == '1') // intersection on bottom
-        {
-            cout << "Bottom" << endl;
-            temp_x = a[0] + (ymin - a[1]) / slope;
-            clipped.push_back({temp_x, ymin});
-        }
-        if (res_or[2] == '1') // intersection on right
-        {
-            cout << "Right" << endl;
-            temp_y = a[1] + (xmax - a[0]) * slope;
-            clipped.push_back({xmax, temp_y});
-        }
-        if (res_or[3] == '1') // intersection on left
-        {
-            cout << "Left" << endl;
-            temp_y = a[1] + (xmin - a[0]) * slope;
-            clipped.push_back({xmin, temp_y});
-        }
+            // completely inside window so accept
+            if (compute_or(c1, c2) == "0000")
+            {
+                accept = true;
+                done = true;
+            }
 
-        return clipped;
+            // completely outside window and ignore
+            else if (compute_and(c1, c2) != "0000")
+                done = true;
+
+            else
+            {
+
+                // clipping from the point which is outside
+                float slope = (b[1] - a[1]) / (b[0] - a[0]);
+
+                string code_out = (c1 != "0000") ? c1 : c2;
+
+                // TBRL check
+                // top intersection
+
+                float temp_x, temp_y;
+
+                if (code_out[0] == '1') // intersection on top
+                {
+                    temp_x = a[0] + (ymax - a[1]) / slope;
+                    temp_y = ymax;
+                }
+                else if (code_out[1] == '1') // intersection on bottom
+                {
+                    temp_x = a[0] + (ymin - a[1]) / slope;
+                    temp_y = ymin;
+                }
+                else if (code_out[2] == '1') // intersection on right
+                {
+                    temp_y = a[1] + (xmax - a[0]) * slope;
+                    temp_x = xmax;
+                }
+                else //  (code_out[3] == '1') // intersection on left
+                {
+                    temp_y = a[1] + (xmin - a[0]) * slope;
+                    temp_x = xmin;
+                }
+                // update the outside pt to intersection
+                if (code_out == c1)
+                {
+                    a[0] = temp_x;
+                    a[1] = temp_y;
+                    c1 = compute_code(a);
+                }
+                else
+                {
+                    b[0] = temp_x;
+                    b[1] = temp_y;
+                    c2 = compute_code(b);
+                }
+            }
+
+        } while (done == false);
+
+        // line accepted
+        if (accept)
+        {
+            cout << "Line accepted / clipped" << endl;
+            setcolor(14);
+            line(a[0], a[1], b[0], b[1]);
+        }
+        else
+            cout << "Line Rejected" << endl;
     }
 
     static string compute_and(string c1, string c2)
@@ -124,33 +151,29 @@ int main()
 
     line_clip P = line_clip(x_wmax, x_wmin, y_wmax, y_wmin);
 
+    // 2 pt intersection
     vector<float> A = {10, 30};
     vector<float> B = {80, 90};
 
-
+    // 1 pt intersection
     vector<float> C = {55, 45};
     vector<float> D = {100, 100};
 
+    // Completely outside
+    vector<float> E = {20, 200};
+    vector<float> F = {100, 100};
 
     setcolor(3);
     line(A[0], A[1], B[0], B[1]);
-    // line(C[0], C[1], D[0], D[1]);
+    line(C[0], C[1], D[0], D[1]);
+    line(E[0], E[1], F[0], F[1]);
     rectangle(x_wmin, y_wmax, x_wmax, y_wmin);
 
     setcolor(14);
-    vector<vector<float>> clipped = P.clip_line(A, B);
-    vector<vector<float>> clipped2 = P.clip_line(D,C);
+    P.clip_line(A, B);
+    P.clip_line(C, D);
+    P.clip_line(E, F);
 
-    if (clipped[0].size() == 1)
-    {
-        cout << "line outside window" << endl;
-        return 0;
-    }
-
-    line(clipped[0][0], clipped[0][1], clipped[1][0], clipped[1][1]);
-    line(clipped2[0][0], clipped2[0][1], clipped2[1][0], clipped2[1][1]);
-
-    line_clip::print_line(clipped2);
     getch();
 
     return 0;
